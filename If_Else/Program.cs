a500b8694561ee739671932b21bec64c887dfd84
Hilal OĞUZHAN <55708261+hilaloguzhan1@users.noreply.github.com>
2022-04-28 13:58:37 +0300

    If-Else

---
+++ If_Else/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int time = DateTime.Now.Hour;
+           if(time<=18)
+           {
+            Console.WriteLine("İyi Günler");
+           }
+           
+           else if(time>6 && time<=11)
+           {
+
+            Console.WriteLine("Günaydın");
+           }
+           else 
+           {
+
+            Console.WriteLine("İyi Geceler");
+           }
+
+           string sonuc = time<=18 ? "İyi Günler":"İyi Geceler";
+           Console.WriteLine(sonuc);
+           string sonuc2 = time>6 && time<=11 ?"Günaydın!": time<=18 ? "İyi Günler":"İyi Geceler";
+           Console.WriteLine(sonuc2);
+
+        }
+    }
+}
