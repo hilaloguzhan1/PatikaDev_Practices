bc5a294645916c9eca3f0d1ec729df3e1e09256f
Hilal OĞUZHAN <55708261+hilaloguzhan1@users.noreply.github.com>
2022-05-10 15:41:37 +0300

    Add files via upload

---
+++ donguler_for/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace donguler_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar=new int[5];
+            Console.WriteLine("Lütfen beş adet sayı giriniz..");
+            Console.Write("Sayı 1=");
+            sayilar[0]=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sayı 2=");
+            sayilar[1]=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sayı 3=");
+            sayilar[2]=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sayı 4=");
+            sayilar[3]=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sayı 5=");
+            sayilar[4]=Convert.ToInt32(Console.ReadLine());
+            
+           
+
+           for (int i = 0; i < sayilar.Length; i++)
+           {
+              //komutlar
+              //Console.WriteLine(sayilar[i]);
+           if(sayilar[i]%2==0)
+           {
+               Console.WriteLine(sayilar[i]+" sayısı 2 ile tam bölünür");
+               
+           }
+           else
+               {
+                  int kalan=sayilar[i]%2; 
+                  Console.WriteLine(sayilar[i]+" Sayısının 2 ye bölümünden kalan "+kalan);
+               }
+            
+
+           }
+        }
+    }
+}
