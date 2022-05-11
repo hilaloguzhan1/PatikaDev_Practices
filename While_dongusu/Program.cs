3e839d96cea6f850a83252e8bef61b75805bbf4a
Hilal OĞUZHAN <55708261+hilaloguzhan1@users.noreply.github.com>
2022-05-11 15:12:14 +0300

    Add files via upload
    While_and_Foreach_dongusu

---
+++ While_dongusu/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace While_dongusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //n kadar girilen sayının(sayı dahil) ortalamasını hesaplayan kod
+            // Console.WriteLine("Lütfen bir sayı giriniz");
+            // int sayi=int.Parse(Console.ReadLine());
+            // int sayac=1;
+            // int toplam=0;
+            // while (sayac<=sayi)
+            // {
+                
+            //     toplam=toplam+sayac;
+            //     sayac++;
+            // }
+            // Console.WriteLine(toplam/sayi);
+
+            // char character='a';
+            // while (character<='z')
+            // {
+            //     Console.WriteLine(character);
+            //     character++;
+            // }
+
+            //Foreach
+            string [] arabalar={"bmw","ford","toyota","nissan"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }  
+    }
+}
